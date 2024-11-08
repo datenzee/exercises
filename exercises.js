f627173f7c6e269563da2ab5f90b3878bddb2021
Jan Slifka <jan.slifka@gmail.com>
2024-11-08 22:40:41 +0100

    Initial commit

---
+++ exercises.js
@@ -0,0 +1,19 @@
+window.exercises = [{
+    title: 'Jumping Jacks',
+    image: 'jumping-jacks.jpg',
+}, {
+    title: 'Table Push-ups',
+    image: 'table-push-ups.webp',
+}, {
+    title: 'Neck Stretches',
+    image: 'neck-stretches.jpg',
+}, {
+    title: 'Squats',
+    image: 'squats.png',
+}, {
+    title: 'Arm Circles',
+    image: 'arm-circles.webp',
+}, {
+    title: 'Wrist Stretches',
+    image: 'wrist-stretches.png',
+}]
---
+++ script.js
@@ -0,0 +1,111 @@
+const defaultTime = 30 * 60;
+const imageQuery = 'fitness sports gym';
+const announceSound = new Audio('announce.wav');
+
+const buttonPlay = document.getElementById('button-play')
+const buttonExercise = document.getElementById('button-exercise')
+const timer = document.querySelector('.timer')
+const timerWrapper = document.querySelector('.timer-wrapper')
+const exercise = document.querySelector('.exercise')
+const exerciseTitle = document.querySelector('.exercise-title')
+const exerciseImage = document.querySelector('.exercise-image')
+const exerciseDone = document.querySelector('.exercise-done')
+
+let exerciseList = shuffleArray(window.exercises)
+let interval
+
+buttonPlay.addEventListener('click', () => {
+    if (interval) {
+        stopTimer()
+    } else {
+        startTimer()
+    }
+})
+
+buttonExercise.addEventListener('click', () => {
+    openExercise()
+})
+
+exerciseDone.addEventListener('click', () => {
+    closeExercise()
+})
+
+function updateTimer(value) {
+    timer.textContent = convertSeconds(value)
+}
+
+function convertSeconds(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+function startTimer() {
+    let time = defaultTime;
+    updateTimer(time);
+    updateBackground();
+    interval = setInterval(() => {
+        time--;
+        updateTimer(time);
+        if (time === 0) {
+            clearInterval(interval);
+            openExercise();
+            playAnnouncement();
+        }
+    }, 1000);
+}
+
+function stopTimer() {
+    if (interval) {
+        clearInterval(interval);
+        interval = null;
+    }
+}
+
+function updateBackground() {
+    fetch(`https://api.unsplash.com/photos/random?query=${imageQuery}&orientation=landscape&client_id=JzZl4koU2EhekuqXX239wbWVdvUhSvHFrSrwte9FDLM`)
+        .then(response => response.json())
+        .then(data => {
+            const imageUrl = data.urls.regular;
+            timerWrapper.style.backgroundImage = `url(${imageUrl})`;
+        })
+        .catch(() => { })
+}
+
+
+function openExercise() {
+    const selectedExercise = getRandomExercise();
+
+    exerciseTitle.textContent = selectedExercise.title;
+    exerciseImage.style.backgroundImage = `url(images/${selectedExercise.image})`;
+
+    exercise.classList.add('visible');
+}
+
+function closeExercise() {
+    exercise.classList.remove('visible');
+}
+
+
+function getRandomExercise() {
+    console.log(exerciseList, exerciseList.length)
+    if (exerciseList.length === 0) {
+        exerciseList = shuffleArray(window.exercises)
+    }
+    return exerciseList.shift()
+}
+
+function shuffleArray(originalArray) {
+    let array = JSON.parse(JSON.stringify(originalArray))
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function playAnnouncement() {
+    announceSound.play();
+}
