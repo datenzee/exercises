04624475dc89a69fbeafac289e7933f247796c2a
Jan Slifka <jan.slifka@gmail.com>
2024-11-09 09:54:09 +0100

    Add more exercises

--- exercises.js
+++ exercises.js
@@ -16,4 +16,25 @@ window.exercises = [{
 }, {
     title: 'Wrist Stretches',
     image: 'wrist-stretches.png',
+}, {
+    title: 'Standing Forward Bend',
+    image: 'standing-forward-bend.png',
+}, {
+    title: 'Standing Quadriceps Stretch',
+    image: 'standing-quadriceps-stretch.jpg',
+}, {
+    title: 'Overhead Stretch',
+    image: 'overhead-stretch.jpg',
+}, {
+    title: 'Calf Stretch Against a Wall',
+    image: 'calf-stretch-against-a-wall.png',
+}, {
+    title: 'Torso Twist',
+    image: 'torso-twist.png',
+}, {
+    title: 'Standing Hamstring Stretch',
+    image: 'standing-hamstring-stretch.jpg',
+}, {
+    title: 'Shoulder Blade Squeeze',
+    image: 'shoulder-blade-squeeze.jpg',
 }]
