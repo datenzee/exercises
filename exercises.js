3b2ea0cbbcdc036bce3e1295aee2b2633887e005
krystofkomanec <krystof.komanec@gmail.com>
2024-11-11 13:35:46 +0100

    Add more excercises

--- exercises.js
+++ exercises.js
@@ -37,4 +37,37 @@ window.exercises = [{
 }, {
     title: 'Shoulder Blade Squeeze',
     image: 'shoulder-blade-squeeze.jpg',
+}, {
+    title: 'Lunge',
+    image: 'lunges.png',
+},{
+    title: 'Leg stretches',
+    image: 'leg-stretch.png',
+},{
+    title: 'Leg lifting',
+    image: 'leg-lift.png',
+},{
+    title: 'Arms up stretch',
+    image: 'arms-up-stretch.png',
+},{
+    title: 'Hip Flexor Stretch',
+    image: 'Low-Back-Exercise-Hip-Flexor-Stretch.jpg',
+},{
+    title: 'Hip Extension',
+    image: 'Lower-Back-Exercise-Hip-Extension-Office1.jpg',
+},{
+    title: 'Low Back Stretch',
+    image: 'Lower-Back-Exercise-Office-Low-Back-Stretch.jpg',
+},{
+    title: 'Pelvic Tilt',
+    image: 'Lower-Back-Exercise-Pelvic-Tilt-Office.jpg',
+},{
+    title: 'Squat Office',
+    image: 'Lower-Back-Exercise-Squat-Office.jpg',
+},{
+    title: 'Arm behind back',
+    image: 'arm-behind-back.png',
+},{
+    title: 'Leg Stretch in Chair',
+    image: 'leg-stretch-in-chair.png',
 }]
