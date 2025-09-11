651f1a5cb859d1bac93cb8d68b94cf8f96a4a8fb
Jan Slifka <jan.slifka@gmail.com>
2025-09-11 09:38:22 +0200

    Remove broken videos

--- videos.js
+++ videos.js
@@ -5,18 +5,6 @@ window.videos = [{
     icon: 'fas fa-fish-fins',
     v: 'QCL7VXuO35g'
 }, {
-    icon: 'fas fa-paw',
-    v: 'Qg4aQUwtr5Q'
-}, {
-    icon: 'fas fa-hat-wizard',
-    v: 'Sagg08DrO5U'
-}, {
     icon: 'fas fa-tree',
     v: 'RzVvThhjAKw'
-}, {
-    icon: 'fas fa-face-grin-tongue',
-    v: 'cwhLueAWItA'
-}, {
-    icon: 'fas fa-fire',
-    v: 'aTKnkQbtyaY'
 }]
